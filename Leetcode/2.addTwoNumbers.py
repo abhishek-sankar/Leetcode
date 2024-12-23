56ed1b4975de7ddbcdb609dc1ab2bed1e3d59434
= <abhishek.sankar.in@gmail.com>
2024-12-22 23:32:45 -0500

    task: Updated 2.addTwoNumbers.py

--- Leetcode/2.addTwoNumbers.py
+++ Leetcode/2.addTwoNumbers.py
@@ -4,11 +4,13 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         head = l3 = ListNode(0)
         carry = 0
         while l1 and l2:
-            l3.val = (l1.val + l2.val + carry) % 10 
+            l3.val = (l1.val + l2.val + carry) % 10
             carry = (l1.val + l2.val + carry) // 10
             l1 = l1.next
             l2 = l2.next
@@ -16,12 +18,12 @@ class Solution:
                 l3_next = ListNode(0)
                 l3.next = l3_next
                 l3 = l3.next
-        
+
         if l1 or l2:
             l3_next = ListNode(0)
             l3.next = l3_next
             l3 = l3.next
-        
+
         while l1:
             l3.val = (l1.val + carry) % 10
             carry = (l1.val + carry) // 10
@@ -39,10 +41,10 @@ class Solution:
                 l3_next = ListNode(0)
                 l3.next = l3_next
                 l3 = l3.next
-        
+
         if carry:
             l3_next = ListNode(carry)
             l3.next = l3_next
-            l3 = l3.next    
+            l3 = l3.next
 
         return head
