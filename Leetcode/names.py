3bf36d2e415a75998f0f469fd850d14fa0d485be
= <abhishek.sankar.in@gmail.com>
2024-12-22 22:13:10 -0500

    chore: cleanup

---
+++ Leetcode/names.py
@@ -0,0 +1,9 @@
+# Bash to get all file names
+# python3 -c "import os; print(sorted(os.listdir('.'), key=lambda x: int(x.split('.')[0]) if x.split('.')[0].isdigit() else float('inf')))"
+
+# Python to set it to main md
+# for f in fil:
+#     # - [36.validSudoku.py](36.validSudoku.py)
+#     print(f"- [{f}]({f})")
+
+# python3 names.py >> names.md
