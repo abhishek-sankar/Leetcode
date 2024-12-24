15e35d962f97bc8d734b31eec8e9feea70c25038
= <abhishek.sankar.in@gmail.com>
2024-12-24 15:43:33 -0500

    task: Updated 41.firstMissingPositive.py

--- Leetcode/41.firstMissingPositive.py
+++ Leetcode/41.firstMissingPositive.py
@@ -1,12 +1,11 @@
-
- '''
- class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        res = set([num for num in nums if num > 0])
-        if len(res) == 0:
-            return 1
-        for i in range(1, max(res)):
-            if i not in res:
-                return i
-        return len(res) + 1
- '''
+"""
+class Solution:
+def firstMissingPositive(self, nums: List[int]) -> int:
+	res = set([num for num in nums if num > 0])
+	if len(res) == 0:
+		return 1
+	for i in range(1, max(res)):
+		if i not in res:
+			return i
+	return len(res) + 1
+"""
