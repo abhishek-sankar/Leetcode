7852b55fb912a4821b72e1bba3d370b112dada0d
= <abhishek.sankar.in@gmail.com>
2024-12-24 00:29:52 -0500

    task: Updated treeDepth.py

--- treeDepth.py
+++ treeDepth.py
@@ -16,3 +16,7 @@ class Graph:
         for neighbour in self.graph_dict[start]:
             if neighbour not in visited:
                 self.dfs(neighbour, visited)
+
+
+graph = Graph([(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (2, 6)])
+graph.dfs(0, set())
