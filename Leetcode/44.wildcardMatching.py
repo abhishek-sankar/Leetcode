7a60c8a5b35140c73fe00d87f4a837770552f8cd
= <abhishek.sankar.in@gmail.com>
2024-12-26 21:29:46 -0500

    task: Updated 44.wildcardMatching.py

---
+++ Leetcode/44.wildcardMatching.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        flag = False
+
+        def dfs(si, pi):
+            print(si, pi)
+            if si >= len(s) and pi >= len(p):
+                flag = True
+                return
+            if pi >= len(p):
+                return
+
+            if s[si] == p[pi]:
+                dfs(si + 1, pi + 1)
+            elif p[pi] == "?":
+                dfs(si + 1, pi + 1)
+            elif p[pi] == "*":
+                if pi + 1 < len(p) and si + 1 < len(s):
+                    if s[si + 1] == p[pi + 1]:
+                        dfs(si + 1, pi + 1)
+                    else:
+                        dfs(si + 1, pi)
+                elif pi == len(p) - 1:
+                    flag = True
+                    return
+                else:
+                    key = True
+                    for j in range(pi, len(p)):
+                        if p[j] != "*":
+                            key = False
+                    if key:
+                        flag = True
+                    return
+
+        dfs(0, 0)
+        return flag
