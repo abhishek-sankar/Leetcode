a75e711a5f599d95aeaf47b8c9bf35a472d4313c
= <abhishek.sankar.in@gmail.com>
2024-12-27 23:23:04 -0500

    task: Updated 119.pascalsTriangle2.py

---
+++ Leetcode/119.pascalsTriangle2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        def getPascalRow(index):
+            if index in cache:
+                return cache[index]
+            else:
+                prevRow = getPascalRow(index - 1)
+                res = [1]
+                for i in range(len(prevRow) - 1):
+                    res.append(prevRow[i] + prevRow[i + 1])
+                res.append(1)
+
+        cache = {0: [1], 1: [1, 1]}
+        return getPascalRow(rowIndex)
