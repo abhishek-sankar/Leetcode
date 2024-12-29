99f0338211f065519da2ae23cf3d6eccf07eda67
= <abhishek.sankar.in@gmail.com>
2024-12-29 16:02:56 -0500

    task: Updated 37.sudokuSolver.py

--- Leetcode/37.sudokuSolver.py
+++ Leetcode/37.sudokuSolver.py
@@ -1,24 +1,5 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        counts = {x: [0] * 27 for x in range(1, 10)}
-        print(counts)
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                if board[i][j] != ".":
-                    counts[int(board[i][j])][int(i)] += 1
-                    if counts[int(board[i][j])][int(i)] > 1:
-                        return False
-
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != ".":
-                    counts[board[i][j]][9 + i] += 1
-                    if counts[board[i][j]][9 + i] > 1:
-                        return False
-
-                if board[j][i] != ".":
-                    counts[board[j][i]][18 + i] += 1
-                    if counts[board[j][i]][18 + i] > 1:
-                        return False
-
-        return True
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
