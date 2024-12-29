b7bd2045bc202916a087b93f6fe37d872bf5d562
= <abhishek.sankar.in@gmail.com>
2024-12-29 16:03:36 -0500

    task: Updated 37.sudokuSolver.py

--- Leetcode/37.sudokuSolver.py
+++ Leetcode/37.sudokuSolver.py
@@ -3,3 +3,8 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+
+        def validSudoku(board):
+            rows = [set() for _ in range(9)]
+            cols = [set() for _ in range(9)]
+            subs = [set() for _ in range(9)]
