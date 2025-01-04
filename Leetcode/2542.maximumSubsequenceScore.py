5cc4fd30383cfe633e7cf9c13465e6936c67ad8c
Abhishek Sankar <abhishek.sankar.in@gmail.com>
2025-01-04 18:17:18 +0530

    02 02 25 (#26)
    * feat: Daily Leetcode
    
    * feat: Daily Leetcode

--- Leetcode/2542.maximumSubsequenceScore.py
+++ Leetcode/2542.maximumSubsequenceScore.py
@@ -4,16 +4,37 @@ class Solution:
         current_sum = 0
         heap = []
         pairs = sorted(zip(nums2, nums1), reverse=True)
-        print(pairs)
-        for mul, num in pairs:
-            heapq.heappush(heap, num)
+        for i in range(k):
+            mul, num = pairs[i]
             current_sum += num
+            heapq.heappush(heap, num)
 
-            if len(heap) > k:
-                num = heapq.heappop(heap)
-                current_sum -= num
-            if len(heap) == k:
-                print(heap, mul)
-                max_sum = max(max_sum, current_sum * mul)
+        max_sum = max(max_sum, current_sum * pairs[k - 1][0])
+        for i in range(k, len(pairs)):
+            num = heapq.heappop(heap)
+            heapq.heappush(heap, pairs[i][1])
+            current_sum = current_sum - num + pairs[i][1]
+            max_sum = max(max_sum, current_sum * pairs[i][0])
 
         return max_sum
+
+
+# class Solution:
+#     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+#         max_sum = float("-inf")
+#         current_sum = 0
+#         heap = []
+#         pairs = sorted(zip(nums2, nums1), reverse=True)
+#         print(pairs)
+#         for mul, num in pairs:
+#             heapq.heappush(heap, num)
+#             current_sum += num
+
+#             if len(heap) > k:
+#                 num = heapq.heappop(heap)
+#                 current_sum -= num
+#             if len(heap) == k:
+#                 print(heap, mul)
+#                 max_sum = max(max_sum, current_sum * mul)
+
+#         return max_sum
